abe7c28e57dfc58c9e74619d909df5cc01a7fcbd
Rota <a.rota@reply.it>
2021-04-10 11:42:21 +0200

    Animat

--- app/src/main/java/com/example/customviewpager/dynamicviewpager/CustomPager.java
+++ app/src/main/java/com/example/customviewpager/dynamicviewpager/CustomPager.java
@@ -2,10 +2,13 @@ package com.example.customviewpager.dynamicviewpager;
 
 import android.content.Context;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 
 /**
  * Created by vihaan on 1/9/15.
@@ -13,6 +16,8 @@ import android.view.View;
 public class CustomPager extends ViewPager {
 
     private View mCurrentView;
+
+    private Boolean mAnimStarted = false;
 
     public CustomPager(Context context) {
         super(context);
@@ -23,16 +28,68 @@ public class CustomPager extends ViewPager {
     }
 
     @Override
-    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        if (mCurrentView == null) {
-            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-            return;
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        if(!mAnimStarted && null != getAdapter()) {
+            int height = 0;
+            View child = ((FragmentPagerAdapter) getAdapter()).getItem(getCurrentItem()).getView();
+            if (child != null) {
+                child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+                height = child.getMeasuredHeight();
+                //if (VersionUtils.isJellyBean() && height < getMinimumHeight()) {
+                    //height = getMinimumHeight();
+                //}
+            }
+
+            // Not the best place to put this animation, but it works pretty good.
+            int newHeight = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+            if (getLayoutParams().height != 0 && heightMeasureSpec != newHeight) {
+                final int targetHeight = height;
+                final int currentHeight = getLayoutParams().height;
+                final int heightChange = targetHeight - currentHeight;
+
+                Animation a = new Animation() {
+                    @Override
+                    protected void applyTransformation(float interpolatedTime, Transformation t) {
+                        if (interpolatedTime >= 1) {
+                            getLayoutParams().height = targetHeight;
+                        } else {
+                            int stepHeight = (int) (heightChange * interpolatedTime);
+                            getLayoutParams().height = currentHeight + stepHeight;
+                        }
+                        requestLayout();
+                    }
+
+                    @Override
+                    public boolean willChangeBounds() {
+                        return true;
+                    }
+                };
+
+                a.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        mAnimStarted = true;
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        mAnimStarted = false;
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                    }
+                });
+
+                a.setDuration(1000);
+                startAnimation(a);
+                mAnimStarted = true;
+            } else {
+                heightMeasureSpec = newHeight;
+            }
         }
-        int height = 0;
-        mCurrentView.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
-        int h = mCurrentView.getMeasuredHeight();
-        if (h > height) height = h;
-        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
 
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
