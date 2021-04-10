2a115e41e69b7fa1e008fc7adb91d5f1a929cb14
Rota <a.rota@reply.it>
2021-04-10 11:39:25 +0200

    First version: no animation, need match parent.

---
+++ app/src/main/java/com/example/customviewpager/dynamicviewpager/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.customviewpager.dynamicviewpager;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.customviewpager.R;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyPagerAdapter pageAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = (ViewPager)findViewById(R.id.myViewPager);
+        pager.setAdapter(pageAdapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
